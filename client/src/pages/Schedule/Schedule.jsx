0bea84a0dfe1b3fd35137f8194c1901b7439fc24
reenan <0renan.souza@gmail.com>
2019-02-28 18:20:55 -0300

    add basic day component

--- client/src/pages/Schedule/Schedule.jsx
+++ client/src/pages/Schedule/Schedule.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react'
 import './Schedule.scss';
 
-import Calendar from './Calendar/Calendar';
-import Events from './Events/Events';
+//import Calendar from './Calendar/Calendar';
+//import Events from './Events/Events';
+import Day from './Day/Day';
 
 export default class Schedule extends Component {
 
 	render() {
 		return (
 			<div className='schedule'>
-				<Calendar />
-				<Events />
+				<Day />	
+			{/* <Calendar />
+				<Events /> */}
 			</div>
 		)
 	}
