1c3582a53638f7e4ab9fb7c369152b5d394bfc34
reenan <0renan.souza@gmail.com>
2019-03-02 16:22:33 -0300

    fix indent calendar

--- client/src/pages/Schedule/Calendar/Calendar.js
+++ client/src/pages/Schedule/Calendar/Calendar.js
@@ -12,157 +12,157 @@ moment.locale('pt-BR');
 
 export default class Calendar extends Component {
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			selectedMonth: 0,
-			selectedYear: 2019
-		}
-	}
+    this.state = {
+      selectedMonth: 0,
+      selectedYear: 2019
+    }
+  }
 
-	static weekDays = ['DOM','SEG','TER','QUA','QUI','SEX','SAB'];
-	static months = [
-		'Janeiro', 
-		'Fevereiro',
-		'Março',
-		'Abril',
-		'Maio',
-		'Junho',
-		'Julho',
-		'Agosto',
-		'Setembro',
-		'Outubro',
-		'Novembro',
-		'Dezembro',
-	];
+  static weekDays = ['DOM','SEG','TER','QUA','QUI','SEX','SAB'];
+  static months = [
+    'Janeiro', 
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ];
 
-	prevMonth = () => {
-		let { selectedMonth, selectedYear } = this.state;
+  prevMonth = () => {
+    let { selectedMonth, selectedYear } = this.state;
 
-		if (selectedMonth !== 0) {
-			selectedMonth--;
-		} else {
-			selectedMonth = 11;
-			selectedYear--;
-		}
+    if (selectedMonth !== 0) {
+      selectedMonth--;
+    } else {
+      selectedMonth = 11;
+      selectedYear--;
+    }
 
-		this.setState({
-			selectedMonth,
-			selectedYear
-		});
-	}
+    this.setState({
+      selectedMonth,
+      selectedYear
+    });
+  }
 
-	nextMonth = () => {
-		let { selectedMonth, selectedYear } = this.state;
+  nextMonth = () => {
+    let { selectedMonth, selectedYear } = this.state;
 
-		if (selectedMonth !== 11) {
-			selectedMonth++;
-		} else {
-			selectedMonth = 0;
-			selectedYear++;
-		}
+    if (selectedMonth !== 11) {
+      selectedMonth++;
+    } else {
+      selectedMonth = 0;
+      selectedYear++;
+    }
 
-		this.setState({
-			selectedMonth,
-			selectedYear
-		});
-	}
+    this.setState({
+      selectedMonth,
+      selectedYear
+    });
+  }
 
-	render() {
-		const { selectedMonth, selectedYear } = this.state;
+  render() {
+    const { selectedMonth, selectedYear } = this.state;
 
-		return (
-			<div className='calendar'>
-				<div className='header'>
-					<p className='selected-year'>{selectedYear}</p>
-					
-					<Icon onClick={this.prevMonth} icon='arrow-left' />
-					<p className='selected-month'>{Calendar.months[selectedMonth]}</p>
-					<Icon onClick= {this.nextMonth}icon='arrow-right' />
-				</div>
+    return (
+      <div className='calendar'>
+        <div className='header'>
+          <p className='selected-year'>{selectedYear}</p>
+          
+          <Icon onClick={this.prevMonth} icon='arrow-left' />
+          <p className='selected-month'>{Calendar.months[selectedMonth]}</p>
+          <Icon onClick= {this.nextMonth}icon='arrow-right' />
+        </div>
 
-				<WeekDays weekDays={Calendar.weekDays} />
-				<Month year={selectedYear} month={selectedMonth} />
-			</div>
-		)
-	}
+        <WeekDays weekDays={Calendar.weekDays} />
+        <Month year={selectedYear} month={selectedMonth} />
+      </div>
+    )
+  }
 }
 
 class Month extends Component {
 
-	MONTH_SIZE = 6 * 7;
+  MONTH_SIZE = 6 * 7;
 
-	addLastMonthPaddingDays = (list) => {
-		const { year, month } = this.props;
-		let currentMonthFirstDay = moment().year(year).month(month).startOf('month').day();
-		let lastMonthDaysCount = moment().year(year).month(month).subtract(1, 'month').daysInMonth();
-		let paddingValue = lastMonthDaysCount - currentMonthFirstDay;
+  addLastMonthPaddingDays = (list) => {
+    const { year, month } = this.props;
+    let currentMonthFirstDay = moment().year(year).month(month).startOf('month').day();
+    let lastMonthDaysCount = moment().year(year).month(month).subtract(1, 'month').daysInMonth();
+    let paddingValue = lastMonthDaysCount - currentMonthFirstDay;
 
-		while (list.length < currentMonthFirstDay) {
-			list.push(
-				<li key={list.length} className='disabled'>
-					<span>{++paddingValue}</span>
-				</li>
-			)
-		}
-	}
+    while (list.length < currentMonthFirstDay) {
+      list.push(
+        <li key={list.length} className='disabled'>
+          <span>{++paddingValue}</span>
+        </li>
+      )
+    }
+  }
 
-	addCurrentMonthDays = (list) => {
-		const { year, month } = this.props;
-		let monthDaysCount = moment().year(year).month(month).daysInMonth();
+  addCurrentMonthDays = (list) => {
+    const { year, month } = this.props;
+    let monthDaysCount = moment().year(year).month(month).daysInMonth();
 
-		for (let i = 1; i <= monthDaysCount; i++) {
-			list.push(
-				<li key={list.length}>
-					<span>{i}</span>
-				</li>
-			)
-		}
-	}
+    for (let i = 1; i <= monthDaysCount; i++) {
+      list.push(
+        <li key={list.length}>
+          <span>{i}</span>
+        </li>
+      )
+    }
+  }
 
-	addNextMonthPaddingDays = (list) => {
-		let paddingValue = 0;
+  addNextMonthPaddingDays = (list) => {
+    let paddingValue = 0;
 
-		while (list.length < this.MONTH_SIZE) {
-			list.push(
-				<li key={list.length} className='disabled'>
-					<span>{++paddingValue}</span>
-				</li>
-			)
-		}
-	}
+    while (list.length < this.MONTH_SIZE) {
+      list.push(
+        <li key={list.length} className='disabled'>
+          <span>{++paddingValue}</span>
+        </li>
+      )
+    }
+  }
 
-	render() {
-		let month = [];
+  render() {
+    let month = [];
 
-		this.addLastMonthPaddingDays(month);
-		this.addCurrentMonthDays(month);
-		this.addNextMonthPaddingDays(month);
+    this.addLastMonthPaddingDays(month);
+    this.addCurrentMonthDays(month);
+    this.addNextMonthPaddingDays(month);
 
-		return (
-			<ul className='month'>
-				{month}
-			</ul>
-		)
+    return (
+      <ul className='month'>
+        {month}
+      </ul>
+    )
 
-	}
+  }
 }
 
 
 const WeekDays = ({ weekDays }) => {
 
-	return (
-		<ul className='weekdays'>
-			{
-				weekDays.map((item, index) => {
-					return (
-						<li key={index}>
-							<span>{item}</span>
-						</li>
-					)
-				})
-			}
-		</ul>
-	)
+  return (
+    <ul className='weekdays'>
+      {
+        weekDays.map((item, index) => {
+          return (
+            <li key={index}>
+              <span>{item}</span>
+            </li>
+          )
+        })
+      }
+    </ul>
+  )
 }
