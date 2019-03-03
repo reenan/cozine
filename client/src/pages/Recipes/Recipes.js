fc2c15830757532ae8279cea34db6810d5489a67
reenan <0renan.souza@gmail.com>
2019-03-03 10:54:31 -0300

    init recipes

--- client/src/pages/Recipes/Recipes.js
+++ client/src/pages/Recipes/Recipes.js
@@ -1,16 +1,67 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
 
+import { Icon } from '../shared';
+
 import './Recipes.scss';
+import ragu from '../../resources/images/ragu.jpg';
+import alfredo from '../../resources/images/alfredo.jpg';
+import lasanha from '../../resources/images/lasanha.jpg';
 
 class Recipes extends Component {
+
 	render() {
+		const recipes = [
+			{
+				name: 'Ragu de shimeji',
+				image: ragu,
+				time: '20 minutos',
+				ingredients: ['Shimeji', 'Vinho tinto', 'Polentina', 'Água', 'Sal'],
+			},
+			{
+				name: 'Massa ao molho Alfredo nome comprido para linebreak',
+				image: alfredo,
+				time: '25 minutos',
+				ingredients: ['Macarrão penne', 'Creme de ricota', 'Molho branco', 'Sal', 'Pimenta do reino'],
+			},
+			{
+				name: 'Lasanha de brócolis',
+				image: lasanha,
+				time: '1 hora',
+				ingredients: ['Massa de lasanha', 'Brócolis', 'Tomate', 'Creme de leite', 'Molho branco'],
+			}
+		]
+
 		return (
 			<div className='recipes'>
-				hello recipes
+				<ul>
+				{
+					recipes.map((recipe, index) => {
+						return (
+							<Recipe key={index} recipe={recipe} />
+						)
+					})
+				}
+				</ul>
 			</div>
 		)
 	}
+}
+
+const Recipe = ({ recipe }) => {
+
+	return (
+		<li>
+			<div className='image' style={{backgroundImage: `url(${recipe.image}`}} />
+			<div className='data'>
+				<p className='name'>{recipe.name}</p>
+				<p className='ingredients'>{recipe.ingredients.join(', ')}</p>
+				
+				<Icon icon='clock' />
+				<span>{recipe.time}</span>
+			</div>
+		</li>
+	)
 }
 
 export default withRouter(Recipes);
