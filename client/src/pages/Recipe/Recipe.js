e1090710a1c82fa4333639a33868f97ec90c5bea
reenan <0renan.souza@gmail.com>
2019-03-07 11:50:03 -0300

    add basic nutrients list

--- client/src/pages/Recipe/Recipe.js
+++ client/src/pages/Recipe/Recipe.js
@@ -1,10 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-
-import { Icon } from '../shared';
-
-import './Recipe.scss';
 import lasanha from '../../resources/images/lasanha.jpg';
+import { Icon } from '../shared';
+import './Recipe.scss';
 
 class Recipe extends Component {
 
@@ -27,6 +25,56 @@ class Recipe extends Component {
         { name: 'Creme de leite', amount: '1', }, 
         { name: 'Molho branco', amount: '300ml', },
       ],
+      nutrients: [
+        { name: 'calories', amount: 602, unit: 'kcal'},
+        { name: 'fat', amount: 32, unit: 'grams', items: [
+          { name: 'satured', amount: 15, unit: 'grams', },
+          { name: 'trans', amount: 0.3, unit: 'grams', },
+        ]},
+        { name: 'cholesterol', amount: 166, unit: 'milligrams', },
+        { name: 'carbohydrate', amount: 35, unit: 'grams', },
+        { name: 'protein', amount: 44, unit: 'grams', },
+        { name: 'vitamin', items: [
+          { name: 'a', amount: 34, unit: 'percentage', },
+          { name: 'c', amount: 25, unit: 'percentage', },
+        ]},
+        { name: 'minerals', items: [
+          { name: 'calcium', amount: 550, unit: 'milligrams', },
+          { name: 'iron', amount: 4, unit: 'milligrams', },
+          { name: 'sodium', amount: 1576, unit: 'milligrams', },
+          { name: 'potassium', amount: 1041, unit: 'milligrams', },
+        ]},
+      ],
+    }
+
+    const RECOMENDED_NUTRIENTS_AMOUNT = {
+      calories: 2000,
+      fat: 65,
+      satured: 20,
+      cholesterol: 300,
+      carbohydrate: 300,
+      calcium: 1100,
+      iron: 14,
+      sodium: 2400,
+      potassium: 4700,
+    }
+
+    const NUTRIENT_MAP = {
+      calories: 'Calorias',
+      fat: 'Gorduras',
+      satured: 'Saturada',
+      trans: 'Trans',
+      cholesterol: 'Colesterol',
+      carbohydrate: 'Carboidratos',
+      protein: 'Proteínas',
+      vitamin: 'Vitaminas',
+      a: 'A',
+      c: 'C',
+      minerals: 'Minerais',
+      calcium: 'Cálcio',
+      iron: 'Ferro',
+      sodium: 'Sódio',
+      potassium: 'Potássio',
     }
 
     const SERVING_ICON_MAP = {
@@ -87,24 +135,37 @@ class Recipe extends Component {
 
           <div className='nutritional-values'>
             <p className='subtitle'>Valores nutricionais</p>
-
             <ul>
-              <li>
-                <p>Carbohydrates</p>
-                <span>120g</span>
-              </li>
-              <li>
-                <p>Protein</p>
-                <span>120g</span>
-              </li>
-              <li>
-                <p>Fat</p>
-                <span>120g</span>
-              </li>
-              <li>
-                <p>Vitamins and Minerals</p>
-                <span>120g</span>
-              </li>
+              {
+                recipe.nutrients.map((nutrient, i) => {
+                  return (
+                    <li key={i}>
+                      <span>{NUTRIENT_MAP[nutrient.name]}</span>
+                      {
+                        nutrient.amount !== undefined ?
+                          <span>{formatNutrientAmount(nutrient.amount, nutrient.unit)}</span> : null
+                      }
+
+                      {
+                        nutrient.items !== undefined ?
+                          <ul>
+                            {
+                              nutrient.items.map((nutrientItem, j) => {
+                                return (
+                                  <li key={j}>
+                                    <span>{NUTRIENT_MAP[nutrientItem.name]}</span>
+                                    <span>{formatNutrientAmount(nutrientItem.amount, nutrientItem.unit)}</span>
+                                  </li>
+                                )
+                              })
+                            }
+                          </ul> : null
+                      }
+
+                    </li>
+                  )
+                })
+              }
             </ul>
 
           </div>
@@ -112,6 +173,21 @@ class Recipe extends Component {
 
         </div>
     )
+  }
+}
+
+function formatNutrientAmount(amount, unit) {
+  switch (unit) {
+    case 'kcal':
+      return `${amount} ${unit}`;
+    case 'grams':
+      return `${amount}gr`;
+    case 'milligrams':
+      return `${amount}mg`;
+    case 'percentage':
+      return `${amount}%`;
+    default:
+      return amount;
   }
 }
 
