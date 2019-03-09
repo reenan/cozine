57b1242f3578009481924433f47ad1199052666f
reenan <0renan.souza@gmail.com>
2019-03-09 09:31:47 -0300

    fix indent

--- client/src/pages/shared/Icon/Icon.js
+++ client/src/pages/shared/Icon/Icon.js
@@ -3,31 +3,30 @@ import PropTypes from 'prop-types';
 import './Icon.scss';
 
 export default class Icon extends Component {
+  
+  static defaultProps = {
+    className: '',
+    size: 0
+  }
+  
+  static propsTypes = {
+    icon: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+    size: PropTypes.number,
+  }
+  
+  render() {
+    const { icon, onClick, className, size } = this.props;
     
-    static defaultProps = {
-        className: '',
-        size: 0
-    }
-
-    static propsTypes = {
-        icon: PropTypes.string.isRequired,
-        className: PropTypes.string,
-        onClick: PropTypes.func,
-        size: PropTypes.number,
-    }
-
-    render() {
-        const { icon, onClick, className, size } = this.props;
-
-        let style = {};
-
-        if (size) {
-          style.fontSize = size + 'px';
-        }
+    let style = {};
     
-
-        return (
-            <span style={style} onClick={onClick} className={`icon ${icon} ${className ? className : ''}`} />
-        )
+    if (size) {
+      style.fontSize = size + 'px';
     }
+    
+    return (
+      <span style={style} onClick={onClick} className={`icon ${icon} ${className ? className : ''}`} />
+    )
+  }
 }
